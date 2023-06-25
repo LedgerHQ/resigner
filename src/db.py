50538a9fd065808087fb7f2214cbf1e8ae1bd3be
rxbryan <rxbryn@gmail.com>
2023-06-25 13:02:06 +0100

    rename 'db' class, remove 'create' method

--- src/db.py
+++ src/db.py
@@ -1,10 +1,9 @@
 import sqlite3
 
-class db:
+
+class Database:
     def __init__(self, path: str = "resigner.db", **kwargs):
         self.connection = sqlite3.connect(path, **kwargs)
 
-    def create(self, schema):
-        self.connection.execute(schema)
 
-Session = db().connection
+Session = Database().connection
