fe95e41013378aa32602a31f76a7216e038d4cbd
rxbryan <rxbryn@gmail.com>
2023-06-26 03:05:59 +0100

    minor updates to main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,31 @@
 from api import SigningService
-from db import Database
-from models import Utxos, SpentUtxos, AggregateSpends, SignedSpends, UTXOS_SCHEMA, SPENT_UTXOS_SCHEMA,\
-AGGREGATE_SPENDS_SCHEMA, SIGNED_SPENDS_SCHEMA
+from config import Configuration
+from policy import Policy
+from psbt import Psbt
+from db import Session
+from models import (
+    Utxos,
+    SpentUtxos,
+    AggregateSpends,
+    SignedSpends,
+    UTXOS_SCHEMA,
+    SPENT_UTXOS_SCHEMA,
+    AGGREGATE_SPENDS_SCHEMA,
+    SIGNED_SPENDS_SCHEMA
+)
+
+from policy import SpendLimit
+
+class 
 
 def main():
-    pass
+    # Create tables
+    Session.execute(UTXOS_SCHEMA)
+    Session.execute(SPENT_UTXOS_SCHEMA)
+    Session.execute(AGGREGATE_SPENDS_SCHEMA)
+    Session.execute(SIGNED_SPENDS_SCHEMA)
+
+    # Todo
 
 
 if __name__ == '__main__':
